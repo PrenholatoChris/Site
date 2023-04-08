1f75c6e4e58f725a7be6779a13b59132e9f164d9
Christian <christianprenholato10@gmail.com>
2023-04-08 16:09:48 -0300

    prototipo da AboutPage

--- src/AboutPage/About.jsx
+++ src/AboutPage/About.jsx
@@ -1,29 +1,58 @@
 import './About.css';
 
 import img1 from "../assets/6.png"
-import img2 from "../assets/7.png"
-import img3 from "../assets/8.png"
+import myImg from "../assets/me.jpg"
 
 import curriculo from "../assets/curriculo.pdf"
 function About() {
 
     return(
         <div id='About' className="About">
+            <div className="container">
 
-            <iframe src={curriculo} frameborder="0" width="100%" height="100%"></iframe>
-
-            <div className='bg-waves'>
-                {/* <img src={img1} alt="" />
-                <img src={img2} alt="" />
-                <img src={img3} alt="" /> */}
+                <h1>About Me</h1>
+                <div className='topo'>
+                    <img src={myImg} alt="A photo of Christian Prenholato" />
+                    <div>
+                        <h2>Resume</h2>
+                        <p>Me chamo Christian, 21 anos, sou um tanto curioso no mundo da tecnologia. Atualmente estou graduando em Ciência da computação na 
+                            Universidade Federal do Espírito Santo (UFES), e no tempo livre estudo Desenvolvimento web. Me interesso bastante pela área da Segurança da 
+                            informação, principalmente em WebHacking.</p>
+                    </div>
+                </div>
+                <div className='baixo'>
+                    <div className="education">
+                        <h2>Education</h2>
+                        <div>
+                            <h3>2019</h3>
+                            <p>High School Completion<br></br>Escola São Geraldo, Espirito Santo</p>
+                        </div>
+                        <div>
+                            <h3>2019-2020</h3>
+                            <p>Technical Course in Electrotechnics - Incomplete<br></br>Instituto Federal do Espírito Santo</p>
+                        </div>
+                        <div>
+                            <h3>2020-2024</h3>
+                            <p>Degree in Computer Science<br></br>Universidade Federal do Espírito Santo (UFES)</p>
+                        </div>
+                    </div>
+                    <div className="languages">
+                        <h2>Languages</h2>
+                        <p>Portuguese</p>
+                        <strong>Fluent</strong>
+                        <p>English</p>
+                        <strong>Intermediate</strong>
+                    </div>
+                    <div className="interests">
+                        <h2>Interests</h2>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         
-        
-        
-        
         </div>
-
-
     )
     
 }
